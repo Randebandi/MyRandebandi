250596c3ec7fae3ac571a46a329d2a5d99ce22ca
Randebandi <47187639+Randebandi@users.noreply.github.com>
2024-11-24 21:35:24 +0100

    Update settings.py

--- myrandebandi/settings.py
+++ myrandebandi/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'juntagrico_godparent',
     'juntagrico',
     'impersonate',
     'crispy_forms',
